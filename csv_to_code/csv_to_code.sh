2d0f27318bef22a1e8c2e9de408944c771c5aaea
René <101405818+blackscreenofdead@users.noreply.github.com>
2025-06-25 15:42:42 +0200

    Update csv_to_code.sh

--- csv_to_code/csv_to_code.sh
+++ csv_to_code/csv_to_code.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+# Ziel-Datei festlegen
+output_file="output.txt"
+
+# Datei leeren oder erstellen
+> "$output_file"
+
+# CSV einlesen und verarbeiten
 cat name.csv | while read line
 do
-        name=$(echo $line | cut -d ';' -f1)
+    name=$(echo "$line" | cut -d ';' -f1)
+    ip=$(echo "$line" | cut -d ';' -f2)
 
-
-echo '  dstaddr {'
-echo '    name = "'$name'"'
-echo '  }'
-echo ''
+    {
+        echo '  dstaddr {'
+        echo '    name = "'$name'"'
+        echo '    ip = "'$ip'"'
+        echo '  }'
+        echo ''
+    } >> "$output_file"
 done
