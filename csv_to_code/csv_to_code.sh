7a4d5807f03ee500302c7493aa4791fd76ed55be
René <101405818+blackscreenofdead@users.noreply.github.com>
2025-06-25 16:21:47 +0200

    Update csv_to_code.sh

--- csv_to_code/csv_to_code.sh
+++ csv_to_code/csv_to_code.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Ziel-Datei festlegen
 output_file="output.txt"
 
-# Datei leeren oder erstellen
 > "$output_file"
 
-# CSV einlesen und verarbeiten
 cat test.csv | while read line
 do
     name=$(echo "$line" | cut -d ';' -f1)
